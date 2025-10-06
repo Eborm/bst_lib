ae23493bbfbdddc2ee928f54e44f8fe3a61e43ca
Eborm <bramwesel@gmail.com>
2025-10-06 16:00:26 +0200

    Add project files.

---
+++ bst_lib/bst_lib.cs
@@ -0,0 +1,22 @@
+﻿namespace bst_lib
+{
+    public class bst_lib
+    {
+        private detection _detection = new detection();
+        private int height = Console.WindowHeight;
+        private int width = Console.WindowWidth;
+
+
+        public bst_lib()
+        {
+            for (int i = 0; i < height; i++)
+            {
+                for (int j = 0; j < width; j++)
+                {
+                    Console.Write("B");
+                }
+            }
+
+        }
+    }
+}
---
+++ bst_lib/helperclass.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bst_lib
+{
+    internal class detection
+    {
+        public int detect_up_down()
+        {
+            int direction = 0;
+            var key = Console.ReadKey(false).Key;
+
+            switch (key.ToString())
+            {
+                case "UpArrow":
+                    direction = -1;
+                    break;
+
+                case "DownArrow":
+                    direction = 1;
+                    break;
+
+                case "Enter":
+                    direction = 2;
+                    break;
+                default:
+                    break;
+            }
+            return direction;
+        }
+
+        public int advanced_detection()
+        {
+            int direction = 0;
+            var key = Console.ReadKey(false).Key;
+
+            switch (key.ToString())
+            {
+                case "UpArrow":
+                    direction = -1;
+                    break;
+
+                case "DownArrow":
+                    direction = 1;
+                    break;
+
+                case "Enter":
+                    direction = 3;
+                    break;
+
+                case "LeftArrow":
+                    direction = -2;
+                    break;
+
+                case "RightArrow":
+                    direction = 2;
+                    break;
+                default:
+                    break;
+            }
+            return direction;
+        }
+    }
+}

c530a42af0bd1bb31dc6c7152f3da90e949ffb92
Eborm <bramwesel@gmail.com>
2025-10-07 23:26:50 +0200

    Core functionality of bst_lib complete - some bug with selecting text where you can select all text

--- bst_lib/bst_lib.cs
+++ bst_lib/bst_lib.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.Security.Cryptography.X509Certificates;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace bst_lib_namespace
 {
@@ -12,7 +13,7 @@ namespace bst_lib_namespace
         //setting up detection and sleepingbeauty classes
         private detection _detection = new detection();
         private sleepingbeauty _sb = new sleepingbeauty();
-        private double x = 0.003;
+        private double x = 0.002;
 
         //startup animation
         private static string startup =
@@ -62,12 +63,18 @@ namespace bst_lib_namespace
         private List<char> bstlibtext = startup.ToList();
 
         //setting up the text functions
-        private Dictionary<Tuple<int, string>, Tuple<string, string>> _TextDictonary = new Dictionary<Tuple<int, string>, Tuple<string, string>> { { new Tuple<int, string>(0, "empty"), new Tuple<string, string>("You called the wrong text rip", "-") } };
+        private static Action voidfunct() { return () => { }; }
+        private Dictionary<int, Tuple<string, Action>> _TextDictonary = new Dictionary<int, Tuple<string, Action>> { };
+        private Dictionary<string, int> _TextKeyDictonary = new Dictionary<string, int> { { "empty", 0 } };
         private int selectedtext = -1;
-        private int textcount = 0;
+        private int textcount = -1;
+        private List<int> write_this_text = new List<int> {0};
+        private List<int> selectable_text = new List<int> {};
+
 
         public bst_lib(System.ConsoleColor BackgroundColor = ConsoleColor.Black, System.ConsoleColor ForegroundColor = ConsoleColor.White)
         {
+            this.add_text("");
             this.BackgroundColor = BackgroundColor;
             this.ForegroundColor = ForegroundColor;
             Console.BackgroundColor = BackgroundColor;
@@ -84,6 +91,7 @@ namespace bst_lib_namespace
 
         public bst_lib(bool disable_startup_animation, System.ConsoleColor BackgroundColor = ConsoleColor.Black, System.ConsoleColor ForegroundColor = ConsoleColor.White)
         {
+            this.add_text("");
             this.BackgroundColor = BackgroundColor;
             this.ForegroundColor = ForegroundColor;
             Console.BackgroundColor = BackgroundColor;
@@ -100,19 +108,150 @@ namespace bst_lib_namespace
             }
         }
     
+        public void add_text(string text)
+        {
+            Action functionname = null;
+            if (functionname == null) functionname = voidfunct();
+            textcount++;
+            _TextDictonary.Add(textcount, new Tuple<string, Action>(text, null));
+        }
         
-        public void add_text(string text, string functionname="-")
+        public void add_text(string text, Action functionname)
         {
+            if (functionname == null) functionname = voidfunct();
             textcount++;
-            _TextDictonary.Add(new Tuple<int, string>(textcount, ""), new Tuple<string, string>(text, functionname));
-        }
-
-        public void add_text(string keyword, string text, string functionname)
-        {
-            textcount++;
-            _TextDictonary.Add(new Tuple<int, string>(textcount, keyword), new Tuple<string, string>(text, functionname));
+            _TextDictonary.Add(textcount, new Tuple<string, Action>(text, functionname));
         }
 
 
+        public void add_text(string keyword, string text, Action functionname)
+        {
+            if (functionname == null) functionname = voidfunct();
+            textcount++;
+            _TextKeyDictonary.Add(keyword, textcount);
+            _TextDictonary.Add(textcount, new Tuple<string, Action>(text, functionname));
+        }
+
+        public void add_text(string keyword, string text)
+        {
+            Action functionname = null;
+            if (functionname == null) functionname = voidfunct();
+            textcount++;
+            _TextKeyDictonary.Add(keyword, textcount);
+            _TextDictonary.Add(textcount, new Tuple<string, Action>(text, functionname));
+        }
+
+        public void set_shown_text(List<int> textindices)
+        {
+            write_this_text = textindices;
+            if (write_this_text.Count == 0) { write_this_text.Add(0); }
+            foreach ( int key in write_this_text)
+            {
+                if (_TextDictonary[key] != null)
+                {
+                    selectable_text.Add(key);
+                }
+            }
+        }
+
+        public void set_shown_text(List<string> textkeys)
+        {
+            foreach (string key in textkeys)
+            {
+                if (_TextKeyDictonary.ContainsKey(key))
+                {
+                    write_this_text.Add(_TextKeyDictonary[key]);
+                }
+            }
+            foreach (int key in write_this_text)
+            {
+                if (_TextDictonary[key] != null)
+                {
+                    selectable_text.Add(key);
+                }
+            }
+            if (write_this_text.Count == 0) { write_this_text.Add(0); }
+        }
+
+        public void display_text()
+        {
+            selectedtext = Math.Clamp(selectedtext, write_this_text[0], write_this_text[write_this_text.Count - 1]);
+            Console.Clear();
+            foreach (int text_key in write_this_text)
+            {
+                if (text_key == selectedtext)
+                {
+                    write_selected_text(_TextDictonary[text_key].Item1);
+                }
+                else
+                {
+                    write_text(_TextDictonary[text_key].Item1);
+                }
+            }
+            int input = _detection.detect_up_down();
+
+            switch (input)
+            {
+                case -1:
+                    selectedtext--;
+                    if (selectable_text.Contains(selectedtext) == false)
+                    {
+                        selectedtext = selectable_text[selectable_text.Count - 1];
+                    }
+                    break;
+                case 1:
+                    selectedtext++;
+                    if (selectable_text.Contains(selectedtext) == false)
+                    {
+                        selectedtext = selectable_text[0];
+                    }
+                    break;
+                case 2:
+                    if (_TextDictonary[selectedtext].Item2 != _TextDictonary[0].Item2)
+                    {
+                        _TextDictonary[selectedtext].Item2?.Invoke();
+                    }
+                    break;
+                default:
+                    break;
+            }
+
+        }
+
+        public void write_text(string text)
+        {
+            List<char> splittext = text.ToList();
+            foreach (char c in splittext)
+            {
+                if (c == '[')
+                {
+                    continue;
+                }
+                else if (c == ']') { continue; }
+                else { Console.Write(c); }
+            }
+            Console.WriteLine();
+        }
+
+        public void write_selected_text(string text)
+        {
+            List<char> splittext = text.ToList();
+            if (string.Join("", splittext).Contains("["))
+            {
+                foreach (char c in splittext)
+                {
+                    if (c == '[')
+                    {
+                        Console.BackgroundColor = ConsoleColor.White;
+                        Console.ForegroundColor = ConsoleColor.Black;
+                    }
+                    else if (c == ']') { Console.ResetColor(); }
+                    else
+                    { Console.Write(c); }
+                }
+                Console.WriteLine();
+            }
+            else { Console.WriteLine(text);}
+        }
     }
 }
--- test_proj/Program.cs
+++ test_proj/Program.cs
@@ -6,7 +6,31 @@ namespace test_proj
     {
         static void Main(string[] args)
         {
-            bst_lib bst = new bst_lib(ConsoleColor.DarkRed, ConsoleColor.Black);
+            bst_lib bst = new bst_lib(true ,ConsoleColor.Black, ConsoleColor.White);
+            bst.add_text("test", "Test [yes]", myfunction);
+            bst.add_text("test2", "Test 2 [no]", myfunction2);
+            bst.add_text("some text");
+            bst.add_text("some text2");
+            bst.set_shown_text(new List<int> {1, 0 ,2, 0, 3, 4});
+            while (true)
+            {
+                bst.display_text();
+            }
+
+        }
+
+        public static void myfunction()
+        {
+            Console.Clear();
+            Console.WriteLine("Test function");
+            Console.ReadLine();
+        }
+
+        public static void myfunction2()
+        {
+            Console.Clear();
+            Console.WriteLine("Test function 2");
+            Console.ReadLine();
         }
     }
 }
